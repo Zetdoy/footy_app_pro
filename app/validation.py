e87b7788e9ca333f4bafdeaf15e2b245bf8ef3a8
Zetdoy <zachary.szydosz123@gmail.com>
2025-09-04 12:03:10 +0200

    Validation

---
+++ app/validation.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9768e4df72febc2e3ace7d86fc3ad974feda61fb9a1a7905ee9c1c04ef4ba6b1
+size 12273
---
+++ scripts/validate_outcome.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5474823c1d56946d6f77b7a189cc0ba4c5bfb9c026918fd40169b0f2239f7857
+size 7649
